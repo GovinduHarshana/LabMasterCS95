40c1e293bffb8d669e256701ec0fc6f64c4f5998
sandalitissera <sandalihesara@gmail.com>
2025-02-20 14:41:10 +0530

    Script Updated
    Weight Pop up Box Script Updated

--- Assets/Scripts Pysics Practical/WeightPopUp.cs
+++ Assets/Scripts Pysics Practical/WeightPopUp.cs
@@ -1,16 +1,66 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class WeightPopup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject popupPanel;
+    public TMP_Text popupMessage;
+    public Button addButton;
+    public Button cancelButton;
+
+    private GameObject selectedWeight;
+
     void Start()
     {
-        
+
+        popupPanel.SetActive(false);
+
+
+        addButton.onClick.AddListener(OnAddButtonClicked);
+        cancelButton.onClick.AddListener(OnCancelButtonClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void ShowAddPopup(GameObject weight)
     {
-        
+        selectedWeight = weight;
+        popupMessage.text = "Do you want to add 0.5kg to the hanger?";
+        popupPanel.SetActive(true);
+    }
+
+
+    public void ShowRemovePopup(GameObject weight)
+    {
+        selectedWeight = weight;
+        popupMessage.text = "Do you want to remove 0.5kg from the hanger?";
+        popupPanel.SetActive(true);
+    }
+
+
+    void OnAddButtonClicked()
+    {
+
+        AddWeightToHanger(selectedWeight);
+        popupPanel.SetActive(false);
+    }
+
+
+    void OnCancelButtonClicked()
+    {
+
+        Destroy(selectedWeight);
+        popupPanel.SetActive(false);
+    }
+
+
+    void AddWeightToHanger(GameObject weight)
+    {
+
+        weight.transform.SetParent(WeightHangerManager.Instance.hangerArea.transform, false);
+        weight.transform.localPosition = Vector3.zero;
+
+
+        WeightHangerManager.Instance.AddWeight(0.5f);
     }
 }
